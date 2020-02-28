094c9abef655c225851a5e415bef5ce94965868d
Rustam <rustam.guseynov@refinitiv.com>
2020-02-28 19:07:04 +0300

    In progress

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
 #include "Common.h"
 #include "test_runner.h"
 
+#include <functional>
 #include <sstream>
+#include <string>
 
 using namespace std;
+
+class ValueExpression : public Expression {
+public:
+  ValueExpression(int value);
+  virtual ~ValueExpression() = default;
+  virtual int Evaluate() const override;
+  virtual string ToString() const override;
+
+private:
+  int _value;
+};
+
+ValueExpression::ValueExpression(int value) : _value(value) {}
+int ValueExpression::Evaluate() const { return _value; }
+string ValueExpression::ToString() const {
+  return "(" + to_string(_value) + ")";
+}
+
+template <typename BinaryOperation, char op>
+class BinaryExpression : public Expression {
+  BinaryExpression(ExpressionPtr expr1, ExpressionPtr expr2);
+  virtual ~BinaryExpression() = default;
+  virtual int Evaluate() const override;
+  virtual string ToString() const override;
+
+private:
+  ExpressionPtr _expr1, _expr2;
+};
+
+template <typename BinaryOperation, char op>
+BinaryExpression<BinaryOperation, op>::BinaryExpression(ExpressionPtr expr1,
+                                                        ExpressionPtr expr2)
+    : _expr1(move(expr1)), _expr2(move(expr2)) {}
+
+template <typename BinaryOperation, char op>
+string BinaryExpression<BinaryOperation, op>::ToString() const {
+  return "(" + _expr1->ToString() + op + _expr2->ToString() + ")";
+}
+
+template <typename BinaryOperation, char op>
+int BinaryExpression<BinaryOperation, op>::Evaluate() const {
+  return BinaryOperation(_expr1->Evaluate(), _expr2->Evaluate());
+}
+
+class SumExpression : public BinaryExpression<plus<int>, '+'> {
+public:
+  virtual ~SumExpression() = default;
+};
+
+class ProductExpression : public BinaryExpression<multiplies<int>, '*'> {
+public:
+  virtual ~ProductExpression() = default;
+};
 
 string Print(const Expression *e) {
   if (!e) {
