0b6088e0b10f21128f22679a8a92e99b1b506040
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-02-29 13:13:46 +0300

    In progress

--- Solution.cpp
+++ Solution.cpp
@@ -2,9 +2,43 @@
 
 using namespace std;
 
-// Этот файл сдаётся на проверку
-// Здесь напишите реализацию необходимых классов-потомков `IShape`
+class ShapeBase : public IShape {
+ public:
+  virtual void SetPosition(Point point) override { _position = point; }
+  virtual Point GetPosition() const override { return _position; }
 
-// Напишите реализацию функции
+  virtual void SetSize(Size size) override { _size = size; }
+  virtual Size GetSize() const override { return _size; }
+
+  virtual void SetTexture(shared_ptr<ITexture> texture) override {
+    _texture = texture;
+  }
+  virtual ITexture* GetTexture() const override { return _texture.get(); }
+
+  virtual unique_ptr<IShape> Clone() const override { return nullptr; }
+  virtual void Draw(Image&) const override = 0;
+
+ protected:
+  Point _position;
+  Size _size;
+  shared_ptr<ITexture> _texture;
+};
+
+class Ellipse : public ShapeBase {
+ public:
+  virtual void Draw(Image&) const override {}
+};
+class Rectangle : public ShapeBase {
+ public:
+  virtual void Draw(Image&) const override {}
+};
+
 unique_ptr<IShape> MakeShape(ShapeType shape_type) {
+  if (shape_type == ShapeType::Ellipse) {
+    return make_unique<Ellipse>();
+  } else if (shape_type == ShapeType::Rectangle) {
+    return make_unique<Rectangle>();
+  } else {
+    throw invalid_argument("Unexpected shape type");
+  }
 }
