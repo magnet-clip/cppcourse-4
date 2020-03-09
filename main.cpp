84d97a14d9ce309d03d845701944c9aad2226fb9
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-03-09 22:15:43 +0300

    In progress

--- main.cpp
+++ main.cpp
@@ -9,27 +9,34 @@
 #include <vector>
 
 using namespace std;
-
-// TODO need custom hasher and comparator
-// struct Domain {
-//   string name;
-//   unordered_set<shared_ptr<Domain>> children;
-// };
+bool LessOrEqualByChunks(const string &a, const string &b);
+bool LessByChunks(const string &a, const string &b);
+struct ChunksComparer {
+  bool operator()(const string &a, const string &b) const {
+    return LessByChunks(a, b);
+  }
+};
 
 struct IDomainContainer {
-  virtual void Add(const string &domain) = 0;
-  virtual bool Contains(string_view domain) const = 0;
+  virtual void Add(const string &url) = 0;
+  virtual void AddMany(const vector<string> &urs) = 0;
+  virtual bool Contains(string_view url) const = 0;
+  virtual vector<string> GetDomains() const = 0;
 };
 
 class SortedDomainContainer : public IDomainContainer {
-public:
-  virtual void Add(const string &domain) override;
-  void Repack();
-  virtual bool Contains(string_view domain) const override;
+ public:
+  virtual void Add(const string &url) override;
+  virtual void AddMany(const vector<string> &urls) override;
+  virtual bool Contains(string_view url) const override;
+  vector<string> GetDomains() const override;
 
-private:
-  set<string> _domains;
-  vector<string> _domains_cache;
+ private:
+  void Repack();
+  set<string, ChunksComparer> _storage;
+
+  // TODO migrate to string_view since all strings are in storage
+  vector<string> _urls;
 };
 
 string ReverseDomains(string_view domain) {
@@ -48,43 +55,143 @@ string ReverseDomains(string_view domain) {
   return res;
 }
 
-void SortedDomainContainer::Add(const string &domain) {
-  _domains.insert(ReverseDomains(domain));
+vector<string> SortedDomainContainer::GetDomains() const {
+  return {_urls.begin(), _urls.end()};
+}
+
+// Is a subdomain of b
+bool IsSubDomain(const string &a, const string &b) {
+  size_t next_a = -1, next_b = -1;
+
+  do {
+    next_a = a.find('.', next_a + 1);
+    next_b = b.find('.', next_b + 1);
+
+    if (a.substr(0, next_a) != b.substr(0, next_b)) {
+      return false;
+    }
+
+    if (next_a == string::npos || next_b == string::npos) {
+      return next_b == string::npos;
+    }
+
+    // if (next_a == string::npos && next_b == string::npos) {
+    //   return true;
+    // } else if (next_a == string::npos) {
+    //   return false;
+    // } else if (next_b == string::npos) {
+    //   return true;
+    // }
+  } while (true);
+}
+
+void SortedDomainContainer::Add(const string &url) {
+  _storage.insert(ReverseDomains(url));
+  Repack();
+}
+
+void SortedDomainContainer::AddMany(const vector<string> &urls) {
+  for (const auto &url : urls) {
+    _storage.insert(ReverseDomains(url));
+  }
+  Repack();
 }
 
 void SortedDomainContainer::Repack() {
-  _domains_cache.assign(_domains.begin(), _domains.end());
+  _urls.assign(_storage.begin(), _storage.end());
 }
 
-bool comp(const string &a, const string &b) {
-  if (a.length() > b.length()) {
-    return b != a.substr(b.length());
-  } else if (b.length() > a.length()) {
-    return a != b.substr(a.length());
-  } else {
-    return a != b;
-  }
+template <class Iterator>
+struct Range {
+  Iterator start;
+  Iterator finish;
+};
+
+void FindEqualRange(Range<vector<string>::const_iterator> &range,
+                    const string &value) {
+  auto [start, finish] =
+      equal_range(range.start, range.finish, value, LessByChunks);
+  range = {start, finish};
+}
+
+void FindEqualRange2(Range<vector<string>::const_iterator> &range,
+                     const string &value) {
+  range = {lower_bound(range.start, range.finish, value, LessByChunks),
+           lower_bound(range.start, range.finish, value, LessOrEqualByChunks)};
+}
+
+bool LessByChunks(const string &a, const string &b) {
+  /*
+  For all a, comp(a,a)==false
+  If comp(a,b)==true then comp(b,a)==false
+  if comp(a,b)==true and comp(b,c)==true then comp(a,c)==true
+  */
+
+  size_t next_a = -1, next_b = -1;
+
+  do {
+    next_a = a.find('.', next_a + 1);
+    next_b = b.find('.', next_b + 1);
+
+    if (a.substr(0, next_a) != b.substr(0, next_b)) {
+      return a.substr(0, next_a) < b.substr(0, next_b);
+    }
+
+    if (next_a == string::npos && next_b == string::npos) {
+      return false;  // they are equal, hence not less
+    } else if (next_a == string::npos) {
+      return true;  // a is shorter than b hence it is less
+    } else if (next_b == string::npos) {
+      return false;  // a is longer than b hence it is greater, not less
+    }
+  } while (true);
+}
+
+bool LessOrEqualByChunks(const string &a, const string &b) {
+  size_t next_a = -1, next_b = -1;
+
+  do {
+    next_a = a.find('.', next_a + 1);
+    next_b = b.find('.', next_b + 1);
+
+    if (a.substr(0, next_a) <= b.substr(0, next_b)) {
+      return true;
+    }
+
+    if (next_a == string::npos && next_b == string::npos) {
+      return false;  // a is not less or equal than b, hence a > b
+    } else if (next_a == string::npos) {
+      return true;  // a is shorter than b hence it is less
+    } else if (next_b == string::npos) {
+      return false;  // a is longer than b hence it is greater, not less
+    }
+  } while (true);
 }
 
 bool SortedDomainContainer::Contains(string_view domain) const {
   size_t next_dot = 0;
   string next_sub_domain;
-  set<string>::iterator start;
-  set<string>::iterator finish;
+
+  Range<vector<string>::const_iterator> items = {_urls.begin(), _urls.end()};
+
   do {
     next_dot = domain.find('.');
     if (next_dot == string::npos) {
       next_sub_domain = domain;
     } else {
-      next_sub_domain = domain.substr(0, next_dot));
-      domain.remove_prefix(next_dot + 1);
+      next_sub_domain = domain.substr(0, next_dot);
+      // domain.remove_prefix(next_dot + 1);
     }
-    tie(start, finish) = equal_range(
-        _domains_cache.begin(), _domains_cache.end(), next_sub_domain, comp);
-    if (start == _domains_cache.end()) {
+
+    FindEqualRange(items, next_sub_domain);
+
+    if (items.start == items.finish) {
       // not found
+      return false;
     } else {
-      // found!
+      if (next_dot == string::npos) {
+        return true;
+      }
     }
   } while (next_dot != string::npos);
   return false;
@@ -96,9 +203,215 @@ void TestRevertDomain() {
   ASSERT_EQUAL(ReverseDomains("mail.yandex.ru"), "ru.yandex.mail");
 }
 
+void TestPrefixSort() {
+  {
+    vector<string> domains{"com", "ru"};
+    vector<string> expected{"com", "ru"};
+
+    sort(domains.begin(), domains.end(), LessByChunks);
+    ASSERT_EQUAL(domains, expected);
+  }
+  {
+    vector<string> domains{"com",     "ru",           "au.list",
+                           "au.pain", "zh.fun.store", "zh.games"};
+    vector<string> expected{"au.list", "au.pain",      "com",
+                            "ru",      "zh.fun.store", "zh.games"};
+
+    sort(domains.begin(), domains.end(), LessByChunks);
+    ASSERT_EQUAL(domains, expected);
+  }
+  {
+    vector<string> domains{
+        "au",          "ru",          "au.list",     "au.pain",
+        "au.list.wow", "au.lint.wow", "au.list.now",
+    };
+    vector<string> expected{
+        "au",          "au.lint.wow", "au.list", "au.list.now",
+        "au.list.wow", "au.pain",     "ru",
+    };
+
+    sort(domains.begin(), domains.end(), LessByChunks);
+    ASSERT_EQUAL(domains, expected);
+  }
+}
+
+void TestLessByChunksIsValidComp() {
+  // For all a, comp(a,a)==false
+
+  ASSERT(!LessByChunks("au", "au"));
+
+  // If comp(a,b)==true then comp(b,a)==false
+  ASSERT(LessByChunks("au", "au.love"));
+  ASSERT(!LessByChunks("au.love", "au"));
+
+  // if comp(a,b)==true and comp(b,c)==true then comp(a,c)==true
+  ASSERT(LessByChunks("au.love", "au.mail"));
+  ASSERT(LessByChunks("au.mail", "au.mailo"));
+  ASSERT(LessByChunks("au.love", "au.mailo"));
+
+  ///
+  // FindEqualRange(range, "au.list.hoho");
+  // ASSERT_EQUAL(*range.start, "au.list");
+  // ASSERT_EQUAL(*range.finish, "au.pain");
+  ASSERT(LessByChunks("au.list", "au.list.hoho"));
+  ASSERT(LessByChunks("au.list.hoho", "au.pain"));
+}
+
+void TestIsSubDomain() {
+  ASSERT(IsSubDomain("au.less", "au"));
+  ASSERT(IsSubDomain("au", "au"));
+  ASSERT(!IsSubDomain("au", "au.less"));
+  ASSERT(!IsSubDomain("au.love", "au.less"));
+  ASSERT(IsSubDomain("au.love.fun", "au.love"));
+  ASSERT(!IsSubDomain("au.love", "au.love.fun"));
+  ASSERT(!IsSubDomain("zh", "au"));
+  ASSERT(!IsSubDomain("zh.love", "au.love"));
+  ASSERT(!IsSubDomain("zh.love.you", "au.love"));
+  ASSERT(!IsSubDomain("zh.love", "au.love.you"));
+}
+
+void TestAddSomeDomains() {
+  SortedDomainContainer sdc;
+  sdc.Add("ru");
+  sdc.Add("zh");
+  sdc.Add("oracle.com");
+  sdc.Add("yandex.ru");
+  vector<string> expected{"com.oracle", "ru", "ru.yandex", "zh"};
+  ASSERT_EQUAL(sdc.GetDomains(), expected);
+}
+
+void TestFindEqualDomains() {
+  SortedDomainContainer sdc;
+  vector<string> input = {"com",     "ru",           "list.au",
+                          "pain.au", "store.fun.zh", "games.zh"};
+  sdc.AddMany(input);
+  vector<string> expected{"au.list", "au.pain",      "com",
+                          "ru",      "zh.fun.store", "zh.games"};
+  ASSERT_EQUAL(sdc.GetDomains(), expected);
+  {
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange(range, "au");
+    ASSERT_EQUAL(*range.start, "au.list");
+    ASSERT_EQUAL(*range.finish, "au.list");
+  }
+  {
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange(range, "au.list");
+    ASSERT_EQUAL(*range.start, "au.list");
+    ASSERT_EQUAL(*range.finish, "au.pain");
+  }
+  {
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange(range, "au.list.hoho");
+    // bad, does not show the previous one. But it's gonna be found on prev
+    // step
+    ASSERT_EQUAL(*range.start, "au.pain");
+    ASSERT_EQUAL(*range.finish, "au.pain");
+  }
+  {
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange(range, "au.pain");
+    ASSERT_EQUAL(*range.start, "au.pain");
+    ASSERT_EQUAL(*range.finish, "com");
+  }
+  {
+    // searching for fun.fun.zh
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange(range, "zh");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT_EQUAL(*range.finish, "zh.fun.store");
+    // iterators are equal => either not found or exact match
+    FindEqualRange(range, "zh.fun");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT_EQUAL(*range.finish, "zh.fun.store");
+    // iterators are equal => either not found or exact match
+    FindEqualRange(range, "zh.fun.fun");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT_EQUAL(*range.finish, "zh.fun.store");
+    // => not found
+  }
+  {
+    // vector<string> expected{"au.list", "au.pain",      "com",
+    //                         "ru",      "zh.fun.store", "zh.games"};
+    // searching for fun.fun.zh
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange2(range, "zh");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT(range.finish == expected.end());
+    // iterators are equal => either not found or exact match
+    FindEqualRange2(range, "zh.fun");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT(range.finish == expected.end());
+    // iterators are equal => either not found or exact match
+    FindEqualRange2(range, "zh.fun.fun");
+    ASSERT_EQUAL(*range.start, "zh.fun.store");
+    ASSERT(range.finish == expected.end());
+    // => not found
+  }
+
+  // TODO
+  // TODO This behavior seems ok for search
+  // TODO
+  {
+    // vector<string> expected{"au.list", "au.pain",      "com",
+    //                         "ru",      "zh.fun.store", "zh.games"};
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange2(range, "au");
+    ASSERT_EQUAL(*range.start, "au.list");
+    ASSERT_EQUAL(*range.finish, "com");
+
+    FindEqualRange2(range, "au.pain");
+    ASSERT_EQUAL(*range.start, "au.pain");
+    ASSERT_EQUAL(*range.finish, "com");
+
+    FindEqualRange2(range, "au.pain.mail");
+    ASSERT_EQUAL(*range.start, "com");
+    ASSERT_EQUAL(*range.finish, "com");
+  }
+  {
+    // vector<string> expected{"au.list", "au.pain",      "com",
+    //                         "ru",      "zh.fun.store", "zh.games"};
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange2(range, "au");
+    ASSERT_EQUAL(*range.start, "au.list");
+    ASSERT_EQUAL(*range.finish, "com");
+
+    FindEqualRange2(range, "au.love");
+    ASSERT_EQUAL(*range.start, "au.pain");
+    ASSERT_EQUAL(*range.finish, "com");
+  }
+  {
+    // vector<string> expected{"au.list", "au.pain",      "com",
+    //                         "ru",      "zh.fun.store", "zh.games"};
+    Range<vector<string>::const_iterator> range = {expected.cbegin(),
+                                                   expected.cend()};
+    FindEqualRange2(range, "com");
+    ASSERT_EQUAL(*range.start, "com");
+    ASSERT_EQUAL(*range.finish, "ru");
+
+    FindEqualRange2(range, "com.russia");
+    ASSERT_EQUAL(*range.start, "ru");
+    ASSERT_EQUAL(*range.finish, "ru");
+  }
+}
+
 void RunAllTests() {
   TestRunner tr;
+  // RUN_TEST(tr, TestStringComparison);
   RUN_TEST(tr, TestRevertDomain);
+  RUN_TEST(tr, TestLessByChunksIsValidComp);
+  RUN_TEST(tr, TestPrefixSort);
+  RUN_TEST(tr, TestIsSubDomain);
+  RUN_TEST(tr, TestAddSomeDomains);
+  RUN_TEST(tr, TestFindEqualDomains);
 }
 
 int main() { RunAllTests(); }
@@ -146,7 +459,8 @@ int main() { RunAllTests(); }
 
 //   for (const string_view domain : domains_to_check) {
 //     if (const auto it =
-//             upper_bound(begin(banned_domains), end(banned_domains), domain);
+//             upper_bound(begin(banned_domains), end(banned_domains),
+//             domain);
 //         it != begin(banned_domains) && IsSubdomain(domain, *prev(it))) {
 //       cout << "Good" << endl;
 //     } else {
