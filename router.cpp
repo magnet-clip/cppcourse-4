79ce4f5c4c135acbbc7bddfc599826ad2901a951
Rustam <rustam.guseynov@thomsonreuters.com>
2020-05-27 21:48:09 +0300

    Bellman-Ford

--- router.cpp
+++ router.cpp
@@ -28,17 +28,17 @@ optional<double> Router::FindDistance(VertexId from, VertexId to) const {
 void Router::BuildAllRoutes() const {
   const auto vertex_count = graph_.GetVertexCount();
   for (VertexId from = 0; from < vertex_count; from++) {
+    auto& route = _routes[from];
+    route[from].distance = 0;
     auto changed = true;
     while (changed) {
       changed = false;
       for (EdgeId edgeId = 0; edgeId < graph_.GetEdgeCount(); edgeId++) {
         const auto edge = graph_.GetEdge(edgeId);
-        auto& route = _routes[from];
-        route[edge.from].distance = 0.0;
         if (route[edge.from].distance < INF) {
           const auto new_weight = route[edge.from].distance + edge.weight;
           if (route[edge.to].distance > new_weight) {
-            route[edge.to].prev = {edge.to};
+            route[edge.to].prev = {edge.from};
             route[edge.to].distance = new_weight;
             changed = true;
           }
@@ -53,15 +53,13 @@ optional<Router::Route> Router::BuildRoute(VertexId from, VertexId to) const {
     return nullopt;
   } else {
     vector<VertexId> path;
-    double total_distance = 0.0;
     auto current = to;
     do {
       path.push_back(current);
       current = *_routes[from][current].prev;
-      total_distance += _routes[from][current].distance;
     } while (current != from);
     path.push_back(from);
 
-    return {{total_distance, {path.rbegin(), path.rend()}}};
+    return {{_routes[from][to].distance, {path.rbegin(), path.rend()}}};
   }
 }
